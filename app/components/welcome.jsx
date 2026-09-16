b11a9f1946ec12619e4114f4a682c053ebd43429
minharaza <109174892+minharaza@users.noreply.github.com>
2026-09-16 13:18:35 -0400

    key is done

--- app/components/welcome.jsx
+++ app/components/welcome.jsx
@@ -28,6 +28,7 @@ export default function Welcome({ onClose }) {
 
       <button type="button" onClick={onClose} aria-label="Close welcome screen" className="welcome-close">
         <img src="/images/Key.png" alt="Close welcome screen" />
+        <img className="welcome-click-hint" src="/images/Click_Me.png" alt="" aria-hidden="true" />
       </button>
 
       <div className="welcome-plant left">
